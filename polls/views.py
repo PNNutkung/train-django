ec0aef8d51d87696522d47fa4d5d299710128c9c
PNNutkung <pipatpol.t@ku.th>
2016-09-08 17:08:12 +0700

    Add new path to polls app

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,10 @@ from django.http import HttpResponse
 
 def index(req):
     return HttpResponse('Hello, world. You\'re at the polls index.')
+
+def detail(req, question_id):
+    res = 'You\'re looking at the results of question %s.'
+    return HttpResponse(res % question_id)
+
+def vote(req, question_id):
+    return HttpResponse('You\'re voting on question %s.' % question_id)
