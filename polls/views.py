b57962b9053ce1aa187438badc7739e9c3827f85
PNNutkung <pipatpol.t@ku.th>
2016-09-08 23:06:10 +0700

    Add question route and 404

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,17 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(req):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, req))
+    return render(req, 'polls/index.html', context)
 
 def detail(req, question_id):
-    return HttpResponse('You\'re looking at question %s.'% question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(req, 'polls/detail.html', {'question': question})
 
 def results(req, question_id):
     res = 'You\'re looking at the results of question %s.'
