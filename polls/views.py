afaddcd4fcc4947ba6c14778aa475370bd83ab51
PNNutkung <pipatpol.t@ku.th>
2016-09-08 17:52:21 +0700

    Add view template to index

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 def index(req):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, req))
 
 def detail(req, question_id):
+    return HttpResponse('You\'re looking at question %s.'% question_id)
+
+def results(req, question_id):
     res = 'You\'re looking at the results of question %s.'
     return HttpResponse(res % question_id)
 
