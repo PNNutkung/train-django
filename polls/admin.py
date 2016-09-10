67715a5f28f0a2fc104e960713e9179268860557
PNNutkung <pipatpol.t@ku.th>
2016-09-10 15:27:42 +0700

    Add Choice model for admin to edit

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,7 @@ This class is for admin only
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
